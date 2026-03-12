171a501b87757971f321f1d1fd929328a0eb9fe6
Zelayita <20240181@ricaldone.edu.sv>
2026-03-12 11:15:31 -0600

    Nuevas cosas

---
+++ Ejercicios-Zelaya-react/src/Components/Textbox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import React from 'react'
+import './Boton.css'
+
+const Boton = ({ texto, clickHere }) => {
+  return (
+    <button className="boton" onClick={clickHere}>    
+        {texto}
+    </button>
+  )
+}   
+
+export default Boton
