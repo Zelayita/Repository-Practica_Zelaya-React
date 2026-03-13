ea80ace9a8575e8fbd07d56777c06d45906efaf7
Zelayita <20240181@ricaldone.edu.sv>
2026-03-13 15:14:48 -0600

    Nuevos Cambios

--- Ejercicios-Zelaya-react/src/App.jsx
+++ Ejercicios-Zelaya-react/src/App.jsx
@@ -3,14 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import './Components/Boton'
+import Boton from'./Components/Boton.jsx'
 
 function App() {
-
+ 
 
   return (
     <>
-      
+       <div>
+        <Boton
+          texto="Aceptar"
+          clickHere={() => alert("Has hecho click en aceptar")}
+        />
+        <Boton
+          texto="Cancelar"
+          clickHere={() => alert("¿Porque los has cancelado?")}
+        />
+        <Boton
+          texto="Click"
+          clickHere={() => alert("Le diste Click")}
+        />
+        <Boton
+          texto="No Click"
+          clickHere={() => alert("No le tenia que haber dado click")}
+        />
+      </div>
+      <div>
+
+      </div>
     </>
   )
 }
--- Ejercicios-Zelaya-react/src/Components/Boton.jsx
+++ Ejercicios-Zelaya-react/src/Components/Boton.jsx
@@ -1,2 +1,13 @@
 import React from 'react'
 
+import './Boton.css'
+
+const Boton = ({ texto, clickHere }) => {
+  return (
+    <button className="boton" onClick={clickHere}>    
+        {texto}
+    </button>
+  )
+}   
+
+export default Boton
