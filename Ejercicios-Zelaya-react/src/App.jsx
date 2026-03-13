a8ee5a60f685304b149f70340a07d070529a9a03
Zelayita <20240181@ricaldone.edu.sv>
2026-03-13 15:39:35 -0600

    Nuevos Cambios

--- Ejercicios-Zelaya-react/src/App.jsx
+++ Ejercicios-Zelaya-react/src/App.jsx
@@ -4,32 +4,64 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Boton from'./Components/Boton.jsx'
+import Navbar from'./Components/Navbar.jsx'
 
 function App() {
  
 
   return (
     <>
-       <div>
-        <Boton
-          texto="Aceptar"
-          clickHere={() => alert("Has hecho click en aceptar")}
-        />
-        <Boton
-          texto="Cancelar"
-          clickHere={() => alert("¿Porque los has cancelado?")}
-        />
-        <Boton
-          texto="Click"
-          clickHere={() => alert("Le diste Click")}
-        />
-        <Boton
-          texto="No Click"
-          clickHere={() => alert("No le tenia que haber dado click")}
-        />
+        <div>
+        <Navbar />
+        </div>
+<div className="mb-2">
+        <p className="text-center">EJERCICIO 1 - COMPONENTES - Zelaya  =)</p>
       </div>
-      <div>
+      <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              Botones
+            </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse show gap2 p-3" data-bs-parent="#accordionExample">
+            <Boton
+            texto="Boton 1"
+              clickHere={() => alert("Has hecho click en Boton 1")}/>
+            <Boton texto="Boton 2"
+              clickHere={() => alert("Has hecho click en Boton 2")}/>
+            <Boton texto="Boton 3"
+              clickHere={() => alert("Has hecho click en Boton 3")}/>
+            <Boton texto="Boton 4"
+              clickHere={() => alert("Has hecho click en Boton 4")}/>
+            <Boton texto="Boton 5"
+              clickHere={() => alert("Has hecho click en Boton 5")}/>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              Inputs de texto
+            </button>
+          </h2>
+          <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
 
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              Accordion Item #3
+            </button>
+          </h2>
+          <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the third item’s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
